3342c0f2da4214078aae1e8177030d500eb4868f
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-03-20 19:17:38 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -142,9 +142,8 @@ def take_attendance():
             st.dataframe(pd.DataFrame(stud_list))
             st.write("Since there are "+ str(cnt) + " unknown faces. It is suggested the professor must take Manual Attendance also")
             st.subheader("Manual Attendance")
-            manual_attdn=st.multiselect("Choose the students to be included:",absent_list)
-            conf=st.button("Confirm")    
-            if conf:  
+            manual_attdn=st.multiselect("Choose the students to be included:",absent_list)    
+            if st.button("Confirm"):  
                 for ma in manual_attdn:
                     a,b=ma.split("_")
                     if a not in stud_list["name"]:
