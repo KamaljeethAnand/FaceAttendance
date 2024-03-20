b3dff63b5ba1010524e801172534f104aaa5a182
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-03-20 20:36:00 +0530

    Add files via upload

--- attendance.py
+++ attendance.py
@@ -54,10 +54,10 @@ def main():
 import dlib
 def _css_to_rect(css):
     return dlib.rectangle(css.left(), css.top(), css.right(), css.bottom())
-if "clkd" not in st.session_state:
-    st.session_state.clkd=False
-def callback():
-    st.session_state.clkd=True
+# if "clkd" not in st.session_state:
+#     st.session_state.clkd=False
+# def callback():
+#     st.session_state.clkd=True
 def take_attendance():
     with open('encoded_people.pickle', 'rb') as filename:
         people = pickle.load(filename)
@@ -145,17 +145,20 @@ def take_attendance():
             st.subheader("Students detected from Uploaded Images are:")
             st.dataframe(pd.DataFrame(stud_list))
             st.write("Since there are "+ str(cnt) + " unknown faces. It is suggested the professor must take Manual Attendance also")
-            st.button("Manual Attendance",on_click=callback)
-        if st.session_state.clkd:
-            st.subheader("Manual Attendance")
-            manual_attdn=st.multiselect("Choose the students to be included:",absent_list)
-            if st.button("Confirm"): 
-                for ma in manual_attdn:
-                    a,b=ma.split("_")
-                if a not in stud_list["name"]:
-                    stud_list["name"].append(a)
-                    stud_list["usn"].append(b)
-                st.subheader("List of Students after Manual Attendance:")
-                st.dataframe(pd.DataFrame(stud_list)) 
+            with st.form("manattdn"):
+                manattdn=st.form_submit_button("Manual Attendance")
+            if manattdn:
+                st.subheader("Manual Attendance")
+                with st.form("abslist"):
+                    manual_attdn=st.multiselect("Choose the students to be included:",absent_list)
+                    conf=st.form_submit_button("Confirm")
+                if conf: 
+                    for ma in manual_attdn:
+                        a,b=ma.split("_")
+                    if a not in stud_list["name"]:
+                        stud_list["name"].append(a)
+                        stud_list["usn"].append(b)
+                    st.subheader("List of Students after Manual Attendance:")
+                    st.dataframe(pd.DataFrame(stud_list)) 
 if __name__ == '__main__':
     main()
