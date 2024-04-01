c71f97f7c1311d39557565005e4ab83254d2844b
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-04-01 15:53:50 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -72,7 +72,7 @@ def main():
             
         st.write(df2)    
                 # Check if each name in df exists in df2
-        df[shname] = df['usn'].isin(df2['usn'])
+        df[shname] = df['Name'].isin(df2['name'])
                 # Replace True with 'P' and False with 'A'
         df[shname] =  df[shname].map({True: 'P', False: 'A'})  
                 # df=df.iloc[:len(df["Name"])]
