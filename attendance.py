ec95fd0676986fe98ddbd637894ce4b941b59bfc
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-05-27 00:08:30 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -171,7 +171,8 @@ if authentication_status:
                 uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"],accept_multiple_files=True)
             # Read in the live image from camera
             elif option == "Take Live Image":
-                uploaded_file = st.camera_input("Choose an image file")    
+                uploaded_file=[]
+                uploaded_file.append(st.camera_input("Choose an image file"))    
             img=[]
             img_np=[]
             if uploaded_file is not None and len(uploaded_file) !=0:
