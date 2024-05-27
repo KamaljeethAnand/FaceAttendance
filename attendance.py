3d88a472ec1494e8acf6ca71b16edbba588d7695
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-05-27 11:41:26 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -110,7 +110,7 @@ if authentication_status:
             for ma in manual_attdn:
                 x=df["Name"]
                 st.write(x)
-                y=[df["Name"].index(ma) for ma in df["Name"]]
+                # y=[df["Name"].index(ma) for ma in df["Name"]]
                 st.write(y)
                 st.write(df.loc[0])
                 st.write(df.loc[df[0]])
