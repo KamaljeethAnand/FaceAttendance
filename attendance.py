9091b3da5f8c9ed8c9c29c9ce534dc4ec59f9a9b
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2024-05-27 11:25:55 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -110,7 +110,7 @@ if authentication_status:
             for ma in manual_attdn:
                 x=df["Name"]
                 st.write(x)
-                st.write(df.loc[0])
+                st.write(df.loc[df["Name"].index(ma)])
                 # absent_list["usn"][absent_list["name"].index(ma)]
 
         totalp = sum(1 for v in df["Name"] if v=="P")
